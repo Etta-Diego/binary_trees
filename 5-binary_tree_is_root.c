375fc4737b53378540b2fb7da5215c49b5cc1915
Etta-Diego <onyinyechiikediego.gmail.com>
2024-02-29 00:11:43 +0100

    create 5-binary_tree_is_root.c file

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,7 +5,7 @@
  * Return: 1 if node is a leaf, otherwise 0
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 	{
@@ -14,10 +14,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 	if (node->left == NULL || node->right == NULL)
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (1);
 	}
 }
