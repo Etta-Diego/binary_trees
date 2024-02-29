dfbeac2835d597a01bfc90431f875e4c2eddc3d9
Etta-Diego <onyinyechiikediego.gmail.com>
2024-02-29 03:00:34 +0100

    9-binary_tree_height.c update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,4 +8,29 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left, right;
 
+	left = 0, right = 0;
+	if (tree != NULL)
+	{
+		if (tree->right != NULL)
+		{
+			right = 1 + binary_tree_height(tree->right);
+		}
+		if (tree->left != NULL)
+		{
+			left = 1 + binary_tree_height(tree->left);
+		}
+		if (left > right)
+		{
+			return (left);
+		}
+		else
+		{
+			return (right);
+		}
+	}
+	else
+		return 0;
+
+}
