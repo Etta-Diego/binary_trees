c0e3abc61fd4c400de4a1c8e550655b9f38742ca
Etta-Diego <onyinyechiikediego.gmail.com>
2024-02-29 02:29:47 +0100

    update 8-binary_tree_postorder.c file

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: size of the tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+
